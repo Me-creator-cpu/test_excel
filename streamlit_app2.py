8fafc77f3997692dc3bbc0c83090c2934f234f28
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-02 11:49:19 +0100

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -18,6 +18,10 @@ if uploaded_file is not None:
   file.sheet_names
   if "tabs" not in st.session_state:
     st.session_state["tabs"] = file.sheet_names
+  if "chosen_id" not in st.session_state:
+    st.session_state["chosen_id"] = chosen_id
 
+if "chosen_id" is in st.session_state:
+  st.session_state["chosen_id"] 
 
 # st.experimental_rerun()
