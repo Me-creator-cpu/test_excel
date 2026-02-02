3806a0be1c6437e89d0e62335c9b2882688e4ce9
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-02 11:28:21 +0100

    Create streamlit_app2.py

---
+++ streamlit_app2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+# https://gist.github.com/asehmi/160109597bca79f7498d0f24d1adaae6
+
+uploaded_file = st.file_uploader("Choose a file", type = 'xlsx')
+#file = pd.ExcelFile("myfile.xlsx")
+
+if uploaded_file is not None:
+  df1 = pd.read_excel(uploaded_file, sheet_name='Ergebnisse', decimal =',')
+  st.dataframe(df1)
+
+if uploaded_file is not None:
+  file = pd.ExcelFile(uploaded_file)
+  st.tabs(file.sheet_names, width="stretch", default=None)
+
+
+# st.experimental_rerun()
