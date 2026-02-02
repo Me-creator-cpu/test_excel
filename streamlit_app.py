ddee5aa42046e5bcee6ac2288c0bff4c791d7726
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-02 10:51:21 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from io import StringIO
+# Import statistics Library
+import statistics
+from openpyxl import load_workbook
+import xlsxwriter
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+uploaded_file = st.file_uploader("Choose a file", type = 'xlsx')
+if uploaded_file is not None:
+    df1 = pd.read_excel(uploaded_file, sheet_name='Ergebnisse', decimal =',')
+    #st.dataframe(df1)
+
+    df2 = pd.read_excel(uploaded_file, sheet_name='Statistik')
+    #st.dataframe(df2)
+
+    df3 = pd.read_excel(uploaded_file, sheet_name='Probe 1', header = [0, 1, 2], decimal =',')
+    #st.dataframe(df3)
+    probe1Max = df3[df3.columns[0]].max()
+    #st.write('Maximalwert Probe 1: ' + str(round(probe1Max, 2)))
+    probe1Min = df3.iloc[-1, 0]
+    #st.write('Minimalwert Probe 1: ' + str(round(probe1Min, 2)))
+
+
+    df4 = pd.read_excel(uploaded_file, sheet_name='Probe 2', header = [0, 1, 2], converters={0:float})
+    #st.dataframe(df4)
+    probe2Max = df4[df4.columns[0]].max()
+    #st.write('Maximalwert Probe 2: ' + str(round(probe2Max, 2)))
+    probe2Min = df4.iloc[-1, 0]
+    #st.write('Minimalwert Probe 2: ' + str(round(probe2Min, 2)))
+
+    st.write("""---""")
+    #st.write('Maximalwert Probe 1: ' + str(round(probe1Max, 4)))
+    #st.write('Minimalwert Probe 1: ' + str(round(probe1Min, 4)))
+    #st.write('Maximalwert Probe 2: ' + str(round(probe2Max, 4)))
+    #st.write('Minimalwert Probe 2: ' + str(round(probe1Min, 4)))
+
+    mittelwertMax = statistics.mean([probe1Max, probe2Max])
+    mittelwertMin = statistics.mean([probe1Min, probe2Min])
+    st.write('Spannung Beginn: ' + str(round(mittelwertMax, 4)))
+    st.write('Spannung Ende: ' + str(round(mittelwertMin, 4)))
+
+    stdevMax = statistics.stdev([probe1Max, probe2Max])
+    stdevMin = statistics.stdev([probe1Min, probe2Min])
+    st.write('STABW Beginn: ' + str(round(stdevMax, 4)))
+    st.write('STABW Ende: ' + str(round(stdevMin, 4)))
+
+    sigmaRelProbe1 = (probe1Min / probe1Max)
+    sigmaRelProbe2 = (probe2Min / probe2Max)
+    st.write('rel Spannung Probe 1: ' + str(round(sigmaRelProbe1, 4)))
+    st.write('rel Spannung Probe 2: ' + str(round(sigmaRelProbe2, 4)))
+
+    mittelwertSigmaRel = statistics.mean([sigmaRelProbe1, sigmaRelProbe2])
+    st.write('rel Spannung Versuchsende: ' + str(round(mittelwertSigmaRel * 100, 4)))
+
+    stdevSigmaRel = statistics.stdev([sigmaRelProbe1, sigmaRelProbe2])
+    st.write('STABW rel Spannung Versuchsende: ' + str(round(stdevSigmaRel, 4)))
+
+    df5 = pd.DataFrame([{'Spannung Beginn':mittelwertMax, 'STABW Beginn': stdevMax, 'Spannung Ende': mittelwertMin, 'STABW Ende': stdevMin, 'rel. Spannung Versuchsende': mittelwertSigmaRel * 100, 'rel. Spannung STABW': stdevSigmaRel}])
+    st.dataframe(df5)
