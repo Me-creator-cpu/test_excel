005c8952080ae145ca25ed70dbe655fcf9f40354
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-05 16:32:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,7 @@ if df_chart is not None:
     )
     df = df_chart.loc[(df_chart['Level from'] >= int(range_level_min)) & (df_chart['Level from'] <= int(range_level_max))]
     total_col = f"Total cost from {range_level_min} to {range_level_max}"
-    total_cost = df.Cost.sum()
+    total_cost = df.Cost.sum().format(thousands=" ", precision=0)
     #total_cost = df.Cost.sum().style.format(thousands=" ", precision=0)
     st.markdown(f":orange-badge[{total_col} : {total_cost}]")
 
