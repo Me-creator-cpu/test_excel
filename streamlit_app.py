705a2dc59ee036f5904bf826c612c78c3a337371
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-05 12:35:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,14 +24,15 @@ excel_loaded=False
 # ======================================================================================================
 # Définitions DataFrame et Excel
 cols_data = ['Name','Type','Skill','Level','Step','Stars','Stock','Star 1','Star 2','Star 3','Star 4','Star 5','Comp 1','Comp 2','Comp 3','Comp 4','Comp 5','Achievement','Needs','Cost to max','Upgradable','RankPower','Rank','Team','URL','URL Mutation','Mutation 1','Mutation 2']
+cols_data_val = ('Name','Type','Skill','Level','Step','Stars','Stock','Star 1','Star 2','Star 3','Star 4','Star 5','Comp 1','Comp 2','Comp 3','Comp 4','Comp 5','Achievement','Needs','Cost to max','Upgradable','RankPower','Rank','Team','URL','URL Mutation','Mutation 1','Mutation 2')
 df_xls = pd.DataFrame(
     {
         "Sample":          ["Worksheet", "DisplayName",     "Range", "SkipRows", "UpToRow", "DisplayColumns"],
-        "Palmons":         ["Palmon",    "Palmons",         "B:N",    1,         40,         cols_data],
-        "EXP":             ["Tableaux",  "Update costs",    "A:C",    1,         302,        ['Level from', 'Level to', 'Cost']            ],
-        "Competencies":    ["Tableaux",  "Competencies",    "H:I",    1,         31,         ['Level from', 'Cost']                        ],
-        "Mutation":        ["Tableaux",  "Mutation costs",  "N:Q",    1,         224,        ['Level', 'Step', 'Substep', 'Cost level']    ],
-        "MaxCosts":        ["Valeurs",   "Valeurs Réf."     "A:B",    0,         5,          ['Cost type', 'Cost']                         ],
+        "Palmons":         ["Palmon",    "Palmons",         "B:N",    1,         40,         cols_data_val                                 ],
+        "EXP":             ["Tableaux",  "Update costs",    "A:C",    1,         302,        ('Level from', 'Level to', 'Cost')            ],
+        "Competencies":    ["Tableaux",  "Competencies",    "H:I",    1,         31,         ('Level from', 'Cost')                        ],
+        "Mutation":        ["Tableaux",  "Mutation costs",  "N:Q",    1,         224,        ('Level', 'Step', 'Substep', 'Cost level')    ],
+        "MaxCosts":        ["Valeurs",   "Valeurs Réf."     "A:B",    0,         5,          ('Cost type', 'Cost')                         ],
     },
     #index=["Worksheet", "DisplayName", "Range", "SkipRows", "UpToRow", "DisplayColumns"]
 )
