84ff688f960201fce2f53a9ee9bd0b8151b137ba
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-05 10:46:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,11 @@ df_pal_data=None
 xls_data_cols='B:N'
 xls_data_rows=42
 cols_data = ['Name','Type','Skill','Level','Step','Stars','Stock','Star 1','Star 2','Star 3','Star 4','Star 5','Comp 1','Comp 2','Comp 3','Comp 4','Comp 5','Achievement','Needs','Cost to max','Upgradable','RankPower','Rank','Team','URL','URL Mutation','Mutation 1','Mutation 2']
+
+df_costs_mut_full=None
+xls_mut_full_cols='A:B'
+xls_mut_full_rows=5
+cols_mut_full = ['Cost type', 'Cost']
 # ======================================================================================================
 
 def write_js_script():
@@ -84,13 +89,17 @@ if uploaded_file is not None:
 
     df_pal_data=get_data_from_excel(uploaded_file,"Palmon",1,xls_data_cols,xls_data_rows)
     st.dataframe(df_pal_data)
-    
+
+    #                get_data_from_excel(xls_file,xls_sheet,skip,rng_cols,rng_rows)
     df_costs_exp=get_data_from_excel(uploaded_file,"Tableaux",1,xls_exp_cols,xls_exp_rows)
     df_costs_comp=get_data_from_excel(uploaded_file,"Tableaux",1,xls_comp_cols,xls_comp_rows)
     df_costs_mut=get_data_from_excel(uploaded_file,"Tableaux",1,xls_mut_cols,xls_mut_rows)
+    df_costs_mut_full=get_data_from_excel(uploaded_file,"Valeurs",1,xls_mut_full_cols,xls_mut_full_rows)
+    
     st.dataframe(df_costs_exp)
     st.dataframe(df_costs_comp)
     st.dataframe(df_costs_mut)
+    st.dataframe(df_costs_mut_full)
 
     
 
@@ -105,7 +114,7 @@ if uploaded_file is not None:
 #st.markdown(hide_st_style, unsafe_allow_html=True)
 write_js_script()
 js_script = """
-    </div><script language='javascript'>alert('Hello workd');</script><div>
+    </div><script language='javascript'>alert('Hello world');</script><div>
     """
 st.markdown(js_script, unsafe_allow_html=True)
 
