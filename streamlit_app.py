d4714a9987a556b6984b80d953b099302b052dfa
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-09 11:59:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -255,9 +255,11 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs([
                             df_xls["DisplayName"][idx_palmon]
                             ])
 with tab1:
-    if df_xls["DataFrame"][idx_palmon] is not None: 
+    if df_xls["DataFrame"][idx_palmon] is not None:
+        df = df_xls["DataFrame"][idx_palmon]
+        #df.loc[(df_chart["Cost"] >= 1)]
         st.header(df_xls["DisplayName"][idx_costs])
-        max_upg=df_xls["DataFrame"][idx_costs]["Level from"].max()
+        max_upg=df.loc[(df_chart["Cost"] >= 1)]["Level from"].max()
         build_chart_bar(df_xls["DataFrame"][idx_costs],'Level from','Cost','Upgrade costs from level:',int(1),int(max_upg))
         #build_chart_bar(df_xls["DataFrame"][idx_costs],'Level from','Cost','Upgrade costs from level:',int(1),int(300))
         with st.expander("Data graph", expanded=False, width="stretch"):
