d9d4da6d0213d001085d184b310c8a1c9ab3e684
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-09 16:51:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,11 @@ column_config={
     "Unused5": None,
     "Unused6": None,
     "Unused7": None,
-    "Unused8": None
+    "Unused8": None,
+    "Cost upgrade":: st.column_config.NumberColumn(
+        "Cost upgrade",
+        format="compact",
+    )
 }
 # ======================================================================================================
 
@@ -273,12 +277,8 @@ def calcul_upgrade_costs(from_lvl=1,to_lvl=300):
 def show_details(palmon,df):
     df_costs = df_xls["DataFrame"][idx_costs]
     max_upg=df_costs.loc[(df_costs["Cost"] >= 1)]["Level from"].max()
-
-    st.markdown(f":orange-badge[Total : {int(calcul_upgrade_costs(240,max_upg))}]")
-    st.markdown(f":orange-badge[max_upg : {max_upg}]")
-    
     filtered_df = df.copy().iloc[palmon]
-    filtered_df['Cost upgrade']=df['Level'].apply(lambda b: int(calcul_upgrade_costs(b,max_upg)) )
+    filtered_df['Cost upgrade']=df['Level'].apply(lambda b: large_num_format(int(calcul_upgrade_costs(b,max_upg))) )
     st.dataframe(
         filtered_df,
         column_config=column_config,
