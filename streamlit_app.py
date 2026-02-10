7c59519ec5a6db8dcb780ccfd3b1374baf52235b
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-10 11:51:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -457,8 +457,8 @@ with tab4:
         build_table_full_costs(df_xls["DataFrame"][idx_val])
         st.divider()
         st.header(df_xls["DisplayName"][idx_stars])
-        df_starts['Total']=df_costs_stars['Unit cost'].apply(lambda b: int(b)*5 )
-        build_table_any(df_starts)        
+        #df_starts['Total']=df_costs_stars['Unit cost'].apply(lambda b: int(b)*5 )
+        build_table_any(df_costs_stars)        
     else:
         file_err()
 with tab5:
