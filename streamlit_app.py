4776fc01825f45d963ccb9de2760e4fd50860740
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-10 09:41:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -350,6 +350,8 @@ def show_details(palmon,df):
         df_costs = df_xls["DataFrame"][idx_costs]
         max_upg=df_costs.loc[(df_costs["Cost"] >= 1)]["Level from"].max()
         filtered_df = df.copy().iloc[palmon]
+        test_len=len(filtered_df)
+        st.markdown(f":orange-badge[test_len : {test_len}]")
         #filtered_df['Cost upgrade']=df['Level'].apply(lambda b: large_num_format(int(calcul_upgrade_costs(b,max_upg))) )
         filtered_df['Cost to max']=df['Level'].apply(lambda b: int(calcul_upgrade_costs(b,max_upg)) )
         filtered_df['Steps']=df['Step'].apply(lambda b: format_stars(b) )
