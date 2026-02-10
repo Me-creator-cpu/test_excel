5f02b7ecba974cad75a459d2ccc8b8f3af282413
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-10 12:06:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -462,7 +462,8 @@ with tab4:
         df_stars=df_xls["DataFrame"][idx_stars].copy
         #df_stars['Total']=df_stars['Unit cost'].apply(lambda b: int(b)*5 )
         #build_table_any(df_stars)       
-        df_xls["DataFrame"][idx_stars]
+        #df_xls["DataFrame"][idx_stars]
+        df_stars
     else:
         file_err()
 with tab5:
