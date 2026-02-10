c2d162bfd4f9a39710aba0fcfef94158ade5ecfa
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-10 15:22:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -365,7 +365,9 @@ def pal_deltail(df):
     row2 = st.columns(2,border=col_border, width="stretch")
 
     with row0[1]:
-        st.image(df.at[0, 'Base preview'], caption=df.at[0, 'Name'])
+        df_t
+    with row0[2]: # remettre à 1
+        st.image(df.at[0, 'URL'], caption=df.at[0, 'Name'])
     with row1[0]:
         df.at[0, 'Type']
     with row1[1]:
