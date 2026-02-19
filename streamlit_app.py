c4fb358031739eb79b5961dde1a30c7b077bb661
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-19 11:31:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -828,7 +828,7 @@ def menu_tab_dashboards():
     df1['Skill']=df1['Skill'].apply(lambda b: icon_skill(b)) 
     df1['Type']=df1['Type'].apply(lambda b: option_type[data_type['Type'].index(b)]+b)
 
-    df2=df1.iloc[:-1,:].sort_values(by=['Skill','Level','Achievement'],ascending=False)
+    df2=df.iloc[:-1,:].sort_values(by=['Skill','Level','Achievement'],ascending=False)
     
     df=df1.iloc[:-1,:].sort_values(by=['Skill','Level','Achievement'],ascending=False,ignore_index=True)
     df_a=df1.iloc[:-1,:].sort_values(by=['Skill','Level','Achievement'],ascending=False,ignore_index=True)
@@ -839,7 +839,7 @@ def menu_tab_dashboards():
 
     df
     df1
-    df2
+    df2[(df2['Skill'] == '🛡 Defend')].head(7)
     
     row_d0 = st.columns(2,border=col_border, width="stretch")
     with row_d0[0]:
