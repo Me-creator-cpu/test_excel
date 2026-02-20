87a09cdb47f288111add254e55b78909889edd0f
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-20 15:44:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -951,7 +951,7 @@ def menu_tab_dashboards():
         df_tcd2=df2.copy()
         df_tcd1=df_tcd1[["Type","Skill","Level"]]
         df_tcd2=df_tcd2[["Type","Skill","Level"]]
-        df_tcd2.set_index('Type').groupby('Type','Skill').apply(lambda x: x['Level'].count(), include_groups=True).to_frame('Level')
+        df_tcd2.set_index('Type').groupby(['Type','Skill']).apply(lambda x: x['Level'].count(), include_groups=True).to_frame('Level')
         #df_tcd2.set_index('Type').groupby('Type').apply(lambda x: x['Level'].sum() / x['Level'].count(), include_groups=True).to_frame('Level')
         
         df_a=df2.copy()
