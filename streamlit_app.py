eb05f14f6670bd648fde4da5f46140c9d7d4d14d
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-24 15:54:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1295,9 +1295,12 @@ def page3():
 def page4():
     #write_coming_soon()
     #texts_trad = read_json_trads('/mount/src/test_excel/textes.json')
-    texts_trad = read_json_trads('./textes.json')
-    test_trad = get_text_trad('en','text_id')
-    write_on_info(test_trad)
+    #texts_trad = read_json_trads('./textes.json')
+    #test_trad = get_text_trad('en','text_id')
+    #write_on_info(test_trad)
+    with open('textes.json', encoding='utf-8', errors='ignore') as f:
+        json_data = json.load(f, strict=False)  
+        json_data
 
 # ======================================================================================================
 #
