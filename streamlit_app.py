277765b0266480a90d7626cbb30e99ae36a6053b
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-25 16:55:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1439,7 +1439,7 @@ with st.sidebar:
     with range_langu[1]:
         pic(data_flags[site_langu],24)
     menu_load_excel()
-    st.session_state.stream=st.toggle("Page sections", True)
+    st.session_state.stream=st.toggle("Check loaded", True)
 
 langu = st.session_state.site_langu
 
