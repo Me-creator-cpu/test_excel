b0ced269642fc4dce534112d6a79b17820a113a8
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-02-27 16:44:00 +0100

    Update pictures.py

--- pictures.py
+++ pictures.py
@@ -1,9 +1,9 @@
 flag_en='https://img.icons8.com/?size=100&id=t3NE3BsOAQwq&format=png&color=000000'
 flag_fr='https://img.icons8.com/?size=100&id=3muzEmi4dpD5&format=png&color=000000'
 
-url_maintenance='https://scontent-cdg4-2.cdninstagram.com/v/t39.30808-6/632313034_122190805178516338_8580667498397042596_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=100&ig_cache_key=MzgyOTQ0NTI2ODQzMDE5MTIzMQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTA4MC5zZHIuQzMifQ%3D%3D&_nc_ohc=9CUwkXAvwyQQ7kNvwEUeiQU&_nc_oc=Adl4KQ39JfzgHhJP82DLmvNVwanWYMjllSvAo3CYNeWo6SSNScuZBDmntao9H9gSRXg&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-cdg4-2.cdninstagram.com&_nc_gid=jgbS-gOsY44BPhZBZhuRuA&oh=00_AfsArBnqm8Qm_KKklrV7twUzlavNPKn7bpYkHMG93C4WZw&oe=69A36204'
-url_menu_boss='https://i.ytimg.com/vi/ka0jFGAPnqQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAuTuC7wOYDNJ4TqjlsXACLakFfwg'
-url_menu_banner_01='https://static.playee.net/playeegp/file-image-banner_game_palmon_survival_tren_game_gamota_800_x450_jpg_1760327364'
+img_maintenance='./data/img_maintenance.jpg'
+img_menu_boss='./data/img_menu_boss.jpg'
+img_menu_banner_01='./data/'
 
 url_logo_01='./data/logo_01.jpg'
 url_logo_02='./data/logo_02.jpg'
