ceb8f87817e2e28ccec2dc22f4f77ddd72fff712
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-03 11:27:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1547,11 +1547,11 @@ def build_graph_donut():
         
         data = [225, 90, 50, 60, 100, 5]
         
-        wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+        wedges, texts = ax.pie(data, wedgeprops=dict(width=1.5), startangle=-40)
         
         bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
         kw = dict(arrowprops=dict(arrowstyle="-"),
-                  bbox=bbox_props, zorder=0, va="center",border=0)
+                  bbox=bbox_props, zorder=0, va="center")
         
         for i, p in enumerate(wedges):
             ang = (p.theta2 - p.theta1)/2. + p.theta1
