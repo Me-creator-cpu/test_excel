c5702d67e4a5832694689220eb25757ec97da173
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-03 14:14:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1223,10 +1223,46 @@ def menu_tab_dashboards():
             build_main_chart(df_tcd2,None,'Type','Level')
             #build_pivot_table(df_tcd3,'Level','Type','Skill')
             #df_tcd2
+    
+        row_d3 = st.columns(2,border=col_border, width="stretch")
+        with row_d3[0]:
+            st.emtpty()
+        with row_d3[1]:
+            build_graph_donut(df_tcd2.Level,df_tcd2.Type,'Nb Palmons per type')
+    
 
     except:
         st.empty()
-        
+
+def build_graph_donut(data,labels,title):
+    #https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
+    #https://www.geeksforgeeks.org/python/donut-chart-using-matplotlib-in-python/
+    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+    
+    recipe = ["225 g flour", "90 g sugar", "1 egg", "60 g butter", "100 ml milk", "1/2 package of yeast"]
+    recipe = labels
+    
+    #data = [225, 90, 50, 60, 100, 5]
+    
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+    
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
+              bbox=bbox_props, zorder=0, va="center")
+    
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = f"angle,angleA=0,angleB={ang}"
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
+    
+    ax.set_title(title)
+    fig
+
 def menu_tab_graph():
     build_graph_select()
 
@@ -1454,7 +1490,7 @@ def pg_options():
         if st.button("Write Text file"):
             test_write_txt(file_txt)
         if st.button("Donut graph"):
-            build_graph_donut()
+            build_graph_donut_test()
 
 def check_github_access():
     if 'REPLICATE_API_TOKEN' in st.secrets.tests:
@@ -1509,7 +1545,7 @@ def test_write_txt(file_txt):
     #    return st.error('write KO', icon='🚨')
       
 # ======================================================================================================
-def build_graph_donut():
+def build_graph_donut_test():
     rowval = st.columns(2,border=False, width="stretch")
     with rowval[0]:
         # create data
