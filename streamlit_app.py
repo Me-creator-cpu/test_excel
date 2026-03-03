599872fb4ece5f1e53d4f75c1683bca426946935
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-03 16:14:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1247,7 +1247,7 @@ def menu_tab_dashboards():
     Salary = df_tcd3['Nb']
 
     range_colors=["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"]
-    colors_test=range_colors.head(len(Employee))
+    colors_test=range_colors[slice(len(Employee))]
     colors_test
     # colors
     colors = ['#FF0000', '#0000FF', '#FFFF00', '#ADFF2F']
