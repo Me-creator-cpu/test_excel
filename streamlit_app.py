da54ac919907190742cdbd81c30d031a3fb82624
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-03 11:18:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1518,8 +1518,6 @@ def build_graph_donut():
         # Create a pieplot
         plt.pie(size_of_groups)
         #figsize(float, float), default: rcParams["figure.figsize"] (default: [6.4, 4.8])
-        #fig = plt.figure(figsize=(3.2, 2.4))
-        #plt.figure(figsize=(3.2, 2.4))
         
         # add a circle at the center to transform it in a donut chart
         my_circle=plt.Circle( (0,0), 0.8, color='white')
@@ -1534,6 +1532,36 @@ def build_graph_donut():
         
         p
         #plt.show()
+
+    with rowval[1]:
+        fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+        
+        recipe = ["225 g flour",
+                  "90 g sugar",
+                  "1 egg",
+                  "60 g butter",
+                  "100 ml milk",
+                  "1/2 package of yeast"]
+        
+        data = [225, 90, 50, 60, 100, 5]
+        
+        wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+        
+        bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+        kw = dict(arrowprops=dict(arrowstyle="-"),
+                  bbox=bbox_props, zorder=0, va="center")
+        
+        for i, p in enumerate(wedges):
+            ang = (p.theta2 - p.theta1)/2. + p.theta1
+            y = np.sin(np.deg2rad(ang))
+            x = np.cos(np.deg2rad(ang))
+            horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+            connectionstyle = f"angle,angleA=0,angleB={ang}"
+            kw["arrowprops"].update({"connectionstyle": connectionstyle})
+            ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                        horizontalalignment=horizontalalignment, **kw)
+        
+        ax.set_title("Matplotlib bakery: A donut")
 
 # ======================================================================================================
 #
