6853551bdd05f83815316c132c06057a84d6eba4
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-03 14:18:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1228,6 +1228,10 @@ def menu_tab_dashboards():
         with row_d3[0]:
             st.emtpty()
         with row_d3[1]:
+            df_tcd2['Type']
+            st.divider()
+            df_tcd2['Level']
+            st.divider()           
             build_graph_donut(df_tcd2['Level'],df_tcd2['Type'],'Nb Palmons per type')
     
 
@@ -1238,10 +1242,7 @@ def build_graph_donut(data,labels,title):
     #https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
     #https://www.geeksforgeeks.org/python/donut-chart-using-matplotlib-in-python/
     fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-    labels
-    st.divider()
-    data
-    st.divider()
+
     recipe = ["225 g flour", "90 g sugar", "1 egg", "60 g butter", "100 ml milk", "1/2 package of yeast"]
     recipe = labels
     
