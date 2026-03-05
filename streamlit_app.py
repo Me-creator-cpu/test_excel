b61123ce9a88f30e7c5ce5d87e8b784faeb97e4a
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-05 17:30:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1061,7 +1061,7 @@ def menu_tab_val():
         df_stars['Stars level']=df_stars['Stars level'].apply(lambda b: format_stars(b) )
         df_stars.at['Total','Unit Cost']=df_stars['Unit Cost'].mean()
         df_stars.at['Total','Total']=df_stars['Total'].sum()
-        df_stars.pop(5)
+        df_stars = df_stars[:-1]
         build_table_any(df_stars)       
 
 def menu_tab_boss():
