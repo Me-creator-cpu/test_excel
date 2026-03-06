6cdbbd7b8b566de2c26940eb941a8574d806cc11
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-06 09:56:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1042,7 +1042,8 @@ def menu_tab_mut():
     df_energy=df.loc[(df['Step'] != 0)]
     df_crystal=df.loc[(df['Step'] == 0)]  
     st.subheader("🟢Energy")
-    range_level_min, range_level_max = build_chart_bar(df_energy,'Level','Cost level','Mutation costs from level:',int(df_energy['Level'].min()),int(df_energy['Level'].max()))
+    #range_level_min, range_level_max = build_chart_bar(df_energy,'Level','Cost level','Mutation costs from level:',int(df_energy['Level'].min()),int(df_energy['Level'].max()))
+    range_level_min, range_level_max = build_chart_bar(df_energy,'Level','Cost level','Mutation costs from level:',int(1),int(100))
     st.subheader("💎Crystals")
     build_chart_bar(df_crystal,'Level','Cost level','Mutation costs from level:',int(df_crystal['Level'].min()),int(df_crystal['Level'].max()),False)
     with st.expander(get_text_trad(site_langu,'data_graph'), expanded=False, width="stretch"):
