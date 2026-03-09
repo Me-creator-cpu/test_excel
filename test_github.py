783fb3733f8e25cf446f63b8477a2d5359a42a07
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-09 15:35:32 +0100

    Update test_github.py

--- test_github.py
+++ test_github.py
@@ -36,9 +36,9 @@ def test_github_issues():
     #https://docs.github.com/en/rest/meta/meta?apiVersion=2022-11-28#get-all-api-versions
     #github_url = 'https://github.com/USERNAME/REPOSITORY/tree/master/FOLDER'  # change USERNAME, REPOSITORY and FOLDER with actual name
 
-    url_test = 'languages'    "OK
+    url_test = 'languages'    #OK
     
-    url_test = 'contents'    "OK, cf https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository--code-samples
+    url_test = 'contents'    #OK, cf https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository--code-samples
     github_url = f'https://api.github.com/repos/{owner}/{repo}/{url_test}' 
     
     #github_url = f"https://api.github.com/user/starred/{owner}/{repo}"
