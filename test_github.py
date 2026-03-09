8cf501af0f13b00239677251a5823b1b869222bd
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-09 15:05:45 +0100

    Update test_github.py

--- test_github.py
+++ test_github.py
@@ -31,9 +31,12 @@ def test_github_issues():
         st.write('List commits')
         st.write(r.json())
 
+    #https://docs.github.com/en/rest/meta/meta?apiVersion=2022-11-28#get-all-api-versions
     #github_url = 'https://github.com/USERNAME/REPOSITORY/tree/master/FOLDER'  # change USERNAME, REPOSITORY and FOLDER with actual name
 
+    url_test = 'hooks'
     github_url = f'https://api.github.com/repos/{owner}/{repo}/{url_test}' 
+    github_url = f"https://api.github.com/user/starred/{owner}/{repo}"
     result = requests.get(github_url, headers=headers, params=params)
     st.write(f'Testing: {url_test}')
     st.write(github_url)
