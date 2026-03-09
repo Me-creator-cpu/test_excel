45a6ff267f57f871905fd378c20ecef03d4f83b3
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-09 11:32:16 +0100

    Create test_github.py

---
+++ test_github.py
@@ -0,0 +1,13 @@
+import requests
+
+#token = os.getenv('GITHUB_TOKEN', '...')
+token = st.secrets.tests.REPLICATE_API_TOKEN
+owner = "Me-creator-cpu"
+repo = "test_excel"
+query_url = f"https://api.github.com/repos/{owner}/{repo}/issues"
+params = {
+    "state": "open",
+    }
+headers = {'Authorization': f'token {token}'}
+r = requests.get(query_url, headers=headers, params=params)
+result = r.json()
