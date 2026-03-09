ff59e1a9ee0b38ed64cfda214f3dfbcb39b3c853
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-09 12:00:47 +0100

    Update test_github.py

--- test_github.py
+++ test_github.py
@@ -7,6 +7,7 @@ def test_github_issues():
     token = st.secrets.tests.REPLICATE_API_TOKEN
     owner = "Me-creator-cpu"
     repo = "test_excel"
+    
     query_url = f"https://api.github.com/repos/{owner}/{repo}/issues"
     params = {
         "state": "open",
@@ -15,5 +16,12 @@ def test_github_issues():
     st.info(f"Send Request...", icon="ℹ️", width="stretch")
     r = requests.get(query_url, headers=headers, params=params)
     st.info(f"Got results...", icon="ℹ️", width="stretch")
+
+    st.info(f"Send Request...", icon="ℹ️", width="stretch")
+    query_url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    params = {}
+    r = requests.get(query_url, headers=headers, params=params)
+    st.info(f"Got results...", icon="ℹ️", width="stretch")
+    
     result = r.json()
     return result
