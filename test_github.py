4742ad1c857fb7ac6f538e45792e8d8eac437665
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-09 16:41:18 +0100

    Update test_github.py

--- test_github.py
+++ test_github.py
@@ -59,7 +59,84 @@ def test_github_issues():
     st.write(pd.json_normalize(str_json))
 
     #https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents--code-samples
-    url_test = 'contents/data/todo.txt'
+    fileName='todo.txt'
+    url_test = f'contents/data/{fileName}'
     github_url = f'https://api.github.com/repos/{owner}/{repo}/{url_test}' 
-    
+    push_to_repo_branch(
+        gitHubFileName=github_url,
+        fileName=fileName,
+        repo_slug=f'{owner}/{repo}',
+        branch=branch,
+        user=owner,
+        token=token
+        )
     return result
+
+def push_to_repo_branch(gitHubFileName, fileName, repo_slug, branch, user, token):
+    '''
+    Push file update to GitHub repo
+    
+    :param gitHubFileName: the name of the file in the repo
+    :param fileName: the name of the file on the local branch
+    :param repo_slug: the github repo slug, i.e. username/repo
+    :param branch: the name of the branch to push the file to
+    :param user: github username
+    :param token: github user token
+    :return None
+    :raises Exception: if file with the specified name cannot be found in the repo
+    '''
+    
+    message = "Automated update " + str(datetime.datetime.now())
+    path = "https://api.github.com/repos/%s/branches/%s" % (repo_slug, branch)
+
+    r = requests.get(path, auth=(user,token))
+    if not r.ok:
+        st.write("Error when retrieving branch info from %s" % path)
+        st.write("Reason: %s [%d]" % (r.text, r.status_code))
+        raise
+    rjson = r.json()
+    treeurl = rjson['commit']['commit']['tree']['url']
+    r2 = requests.get(treeurl, auth=(user,token))
+    if not r2.ok:
+        st.write("Error when retrieving commit tree from %s" % treeurl)
+        st.write("Reason: %s [%d]" % (r2.text, r2.status_code))
+        raise
+    r2json = r2.json()
+    sha = None
+
+    for file in r2json['tree']:
+        # Found file, get the sha code
+        if file['path'] == gitHubFileName:
+            sha = file['sha']
+
+    # if sha is None after the for loop, we did not find the file name!
+    if sha is None:
+        st.write("Could not find " + gitHubFileName + " in repos 'tree' ")
+        raise Exception
+
+    with open(fileName) as data:
+        content = base64.b64encode(data.read())
+
+    # gathered all the data, now let's push
+    inputdata = {}
+    inputdata["path"] = gitHubFileName
+    inputdata["branch"] = branch
+    inputdata["message"] = message
+    inputdata["content"] = content
+    if sha:
+        inputdata["sha"] = str(sha)
+
+    updateURL = f"https://api.github.com/repos/{repo_slug}/contents/" + gitHubFileName
+    try:
+        rPut = requests.put(updateURL, auth=(user,token), data = json.dumps(inputdata))
+        if not rPut.ok:
+            st.write("Error when pushing to %s" % updateURL)
+            st.write("Reason: %s [%d]" % (rPut.text, rPut.status_code))
+            raise Exception
+    except requests.exceptions.RequestException as e:
+        st.write('Something went wrong! I will print all the information that is available so you can figure out what happend!')
+        st.write(rPut)
+        st.write(rPut.headers)
+        st.write(rPut.text)
+        st.write(e)
+
