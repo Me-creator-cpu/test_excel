5554da38412ddec9f59a70251ff54abd7fb28077
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-11 14:09:56 +0100

    Test

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 def greet():
     return "Hello!"
 
 action = "greet"
 st.write(eval(action + "()"))
+
+test_data=np.random.randn(10, 20) #('col %d' % i for i in range(20)))
+test_data
