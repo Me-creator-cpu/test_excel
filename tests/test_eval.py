529039bee3fedd5487850f7749b1da685782621d
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-11 14:14:41 +0100

    Test

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -9,5 +9,5 @@ action = "greet"
 st.write(eval(action + "()"))
 
 test_data=np.random.randn(10, 1) #('col %d' % i for i in range(20)))
-test_data=('col %d' % i for i in range(20)))
+test_data=('col %d' % i for i in range(20))
 test_data
