46f0b03bebf9553822f549cd6216e0dc40b62eb0
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-13 10:30:44 +0100

    upload_to_github

--- tests/test2_github.py
+++ tests/test2_github.py
@@ -14,6 +14,42 @@ def write_data(sInfo, data):
     with st.expander(f'{sInfo}', expanded=False, icon=':material/table_view:', width='stretch'):
         data
 
+def upload_to_github(github_token: str,
+                     source_file: str, destination_folder: str,
+                     github_repo: str, git_branch: str) -> None:
+    """
+    Uploads a file to a GitHub Pages repository using the PyGithub library.
+    Parameters:
+        github_token: Github token for authentication
+        source_file: The path of the local file to be uploaded
+        destination_folder: The path of the folder in the GitHub repository where the file will be uploaded
+        github_repo: The name of the GitHub repository
+        git_branch: The name of the branch in the GitHub repository
+    """
+    #from github import Github
+
+    # Create a Github instance using token
+    g = Github(github_token)
+    # Get the repository object
+    #repo = g.get_user().get_repo(github_repo)
+    repo = g.get_repo(github_repo)
+    # Get the branch object
+    branch = repo.get_branch(git_branch)
+    # Create the path of the file in the GitHub repository
+    path = destination_folder + "/" + source_file.split("/")[-1]
+    # Create or update the file in the GitHub repository
+    try:
+        # Get the existing file details if it exists
+        existing_file = repo.get_contents(path, ref=branch.name)
+        # Update the file
+        repo.update_file(path, "Update file", open(source_file, 'rb').read(), existing_file.sha,
+                         branch=branch.name)
+        st.write(f"File '{path}' updated successfully.")
+    except Exception as e:
+        # If the file does not exist, create it
+        repo.create_file(path, "Upload file", open(source_file, 'rb').read(), branch=branch.name)
+        st.write(f"File '{path}' created successfully.")
+
 #github_token = st.secrets.tests.REPLICATE_API_TOKEN
 github_token = st.secrets.tests.CLASSIC_TOKEN
 usr_name = 'Me-creator-cpu'
@@ -33,8 +69,12 @@ repo = g.get_repo(f"{usr_name}/{repo_name}")
 contents = repo.get_contents("test_api.txt")
 st.write(contents)
 
-ContentFile, Commit=repo.create_file("test.txt", "test", "test", branch=branch)
-
+#ContentFile, Commit=repo.create_file("test.txt", "test", "test", branch=branch)
+upload_to_github(github_token=github_token,
+                 source_file='test_api_2.txt',
+                 destination_folder='',
+                 github_repo=f"{usr_name}/{repo_name}",
+                 git_branch=branch)
 
 # Get all pull requests (adjust state and number parameters as needed)
 pull_requests = repo.get_pulls(state="open", sort="created", direction="desc")
