1fa461fd6b5dcca41a12e1b57c2572645c4dbd36
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-13 09:05:01 +0100

    Test

--- tests/test2_github.py
+++ tests/test2_github.py
@@ -19,6 +19,8 @@ usr_name = 'Me-creator-cpu'
 repo_name = 'test_excel'
 branch = 'main'
 
+st.write(f"{github_token}")
+
 auth = Auth.Token(github_token)  
 
 # Create a GitHub object with your access token
@@ -52,6 +54,9 @@ for pull_request in pull_requests:
     st.write(f"Deleted lines: {deletions}")
     st.write(f"Total lines changed: {additions + deletions}")
 
+query_url = f"https://api.github.com/repos/{usr_name}/{repo_name}/commits"
+params = {}
+r = requests.get(query_url, headers=headers, params=params)
 issues = g.search_issues(
             query=f'is:pr is:merged base:master repo:”{usr_name}/{repo_name}” created:>2026-03-01',
             sort='updated',
