d4c7da2ae02b0f60bba0f7c1593cc09c7551725e
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-17 16:59:12 +0100

    df3

--- git_translations.py
+++ git_translations.py
@@ -117,13 +117,6 @@ def json_to_frame():
     subTitle('df')
     df = pd.DataFrame(json_data['data'])
     df
-
-    #edited_df = st.data_editor(df.T)
-    subTitle('df2')
-    df2=df.T
-    df2
-
-
     
     subTitle('df3')
     df3=df.T.copy()
