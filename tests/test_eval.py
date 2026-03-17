f53d48786cf348005f8f1f7468906adbe19f0d51
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-17 12:04:03 +0100

    Test

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -11,3 +11,8 @@ st.write(eval(action + "()"))
 test_data=np.random.randn(10, 1) #('col %d' % i for i in range(20)))
 test_data=('tab%d' % i for i in range(5))
 test_data
+
+
+if 'site_langu' in st.session_state:
+    site_langu=st.session_state.site_langu
+    st.write(f'site_langu={site_langu}')
