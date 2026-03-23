aa367946db077a1036d3b1ad9dada9cc2b78cb51
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-23 09:51:03 +0100

    df_change

--- git_translations.py
+++ git_translations.py
@@ -201,7 +201,8 @@ def json_langu(val_langu,langu):
     return ret_val
 
 def df_change():
-    if 'updated_trad' in st.session_state:
+    if 'dek' in st.session_state:
+    #if 'updated_trad' in st.session_state:
         edited_rows = st.session_state.updated_trad['edited_rows']    
         st.toast('editor_df on_change', icon='ℹ️️', duration='short')
 
