f0b3d367dada945575098595719ce3da997787ae
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-23 17:25:36 +0100

    calculated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1812,9 +1812,9 @@ def calc_dreamium_final(df_source,df_input):
     
     for i in df_input:
         for l in rows:
-            calc_qty=df_source["quantity"][l]
+            calc_qty=df_source["calculated"][l]
             #df_source["quantity"][l] +=df_input[i]["quantity"]*(4**(i-l)) if l!=i else 0
-            df_source["quantity"][l]=calc_qty + df_input[i]["quantity"]*(4**(i-l))
+            df_source["calculated"][l]=calc_qty + df_input[i]["quantity"]*(4**(i-l))
     st.write('df_source')
     df_source
 
