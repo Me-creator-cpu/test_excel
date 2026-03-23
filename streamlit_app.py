207b47e784f2909bda1c438b64b36869227a286e
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-23 09:34:19 +0100

    st.navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -2087,11 +2087,11 @@ pages = {
         st.Page(page3, title=get_text_trad(site_langu,'menu_info_file'),icon="📋"),
     ],
     get_text_trad(site_langu,'menu_param'): [
-        st.Page(pg_options, title=get_text_trad(site_langu,'menu_options'),icon="⚙️"), #🛠️
-        st.Page('./git_translations.py', title=get_text_trad(site_langu,'menu_git_translate'),icon="🛠️"),
-        st.Page("./tests/test_eval.py", title="Tests",icon="🛠️"),
-        st.Page("./tests/test2_github.py", title="Test Github",icon="🛠️"),
-        st.Page(page_tests, title="Test Python",icon="🛠️")
+        st.Page(pg_options, title=get_text_trad(site_langu,'menu_options'),icon='⚙️'), #🛠️
+        st.Page('./git_translations.py', title=get_text_trad(site_langu,'menu_git_translate'),icon='🛠️'),
+        st.Page('./tests/test_eval.py', title='Tests',icon='🛠️'),
+        st.Page('./tests/test2_github.py', title='Test Github',icon='🛠️'),
+        st.Page(page_tests, title='Test Python',icon='🛠️')
     ],    
 }
 pg = st.navigation(
