ff4164ad3413b35c0ced06cbfb8712852dfdb2d3
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-24 17:10:34 +0100

    lambda:

--- streamlit_app.py
+++ streamlit_app.py
@@ -421,16 +421,16 @@ def test_dummy():
     st.toast(f"Building Menu v2.")
     menu_v2_idx_palmon=st.Page(lambda: menu_tab_show(idx_palmon), title="Full list", icon=":material/security:")
     
-    menu_v2_idx_costs=st.Page(menu_tab_show(idx_costs), title="Upgrade costs", icon=":material/security:")
-    menu_v2_idx_comp=st.Page(menu_tab_show(idx_comp), title="Competencies", icon=":material/security:")
-    menu_v2_idx_mut=st.Page(menu_tab_show(idx_mut), title="Mutation costs", icon=":material/security:")
+    menu_v2_idx_costs=st.Page(lambda: menu_tab_show(idx_costs), title="Upgrade costs", icon=":material/security:")
+    menu_v2_idx_comp=st.Page(lambda: menu_tab_show(idx_comp), title="Competencies", icon=":material/security:")
+    menu_v2_idx_mut=st.Page(lambda: menu_tab_show(idx_mut), title="Mutation costs", icon=":material/security:")
     
-    menu_v2_idx_val=st.Page(menu_tab_show(idx_val), title="Upgrade full costs", icon=":material/security:")
-    menu_v2_idx_equip=st.Page(menu_tab_show(idx_equip), title="Equipments", icon=":material/security:")
-    menu_v2_idx_equip_nov=st.Page(menu_tab_show(idx_equip_nov), title="Equipments Explorer", icon=":material/security:")
+    menu_v2_idx_val=st.Page(lambda: menu_tab_show(idx_val), title="Upgrade full costs", icon=":material/security:")
+    menu_v2_idx_equip=st.Page(lambda: menu_tab_show(idx_equip), title="Equipments", icon=":material/security:")
+    menu_v2_idx_equip_nov=st.Page(lambda: menu_tab_show(idx_equip_nov), title="Equipments Explorer", icon=":material/security:")
 
-    menu_v2_idx_boss=st.Page(menu_tab_show(idx_boss), title="Boss", icon=":material/security:")
-    menu_v2_idx_boss_data=st.Page(menu_tab_show(boss_data), title="Boss data", icon=":material/security:")
+    menu_v2_idx_boss=st.Page(lambda: menu_tab_show(idx_boss), title="Boss", icon=":material/security:")
+    menu_v2_idx_boss_data=st.Page(lambda: menu_tab_show(boss_data), title="Boss data", icon=":material/security:")
 
     menu_v2_m0=[menu_v2_idx_palmon]
     menu_v2_m50=[menu_v2_idx_costs,menu_v2_idx_comp,menu_v2_idx_mut]
