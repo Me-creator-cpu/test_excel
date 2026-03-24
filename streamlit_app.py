e24d257c4b3f3b9585dfd79359853445d5927d2b
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-24 17:29:21 +0100

    test_dummy

--- streamlit_app.py
+++ streamlit_app.py
@@ -421,29 +421,29 @@ def test_dummy():
     st.toast(f"Building Menu v2.")
     menu_v2_idx_palmon=st.Page(lambda: menu_tab_show(idx_palmon), title="Full list", icon=":material/security:")
     
-    menu_v2_idx_costs=st.Page(lambda: menu_tab_show(idx_costs), title="Upgrade costs", icon=":material/security:")
-    menu_v2_idx_comp=st.Page(lambda: menu_tab_show(idx_comp), title="Competencies", icon=":material/security:")
-    menu_v2_idx_mut=st.Page(lambda: menu_tab_show(idx_mut), title="Mutation costs", icon=":material/security:")
+    #menu_v2_idx_costs=st.Page(lambda: menu_tab_show(idx_costs), title="Upgrade costs", icon=":material/security:")
+    #menu_v2_idx_comp=st.Page(lambda: menu_tab_show(idx_comp), title="Competencies", icon=":material/security:")
+    #menu_v2_idx_mut=st.Page(lambda: menu_tab_show(idx_mut), title="Mutation costs", icon=":material/security:")
     
-    menu_v2_idx_val=st.Page(lambda: menu_tab_show(idx_val), title="Upgrade full costs", icon=":material/security:")
-    menu_v2_idx_equip=st.Page(lambda: menu_tab_show(idx_equip), title="Equipments", icon=":material/security:")
-    menu_v2_idx_equip_nov=st.Page(lambda: menu_tab_show(idx_equip_nov), title="Equipments Explorer", icon=":material/security:")
+    #menu_v2_idx_val=st.Page(lambda: menu_tab_show(idx_val), title="Upgrade full costs", icon=":material/security:")
+    #menu_v2_idx_equip=st.Page(lambda: menu_tab_show(idx_equip), title="Equipments", icon=":material/security:")
+    #menu_v2_idx_equip_nov=st.Page(lambda: menu_tab_show(idx_equip_nov), title="Equipments Explorer", icon=":material/security:")
 
-    menu_v2_idx_boss=st.Page(lambda: menu_tab_show(idx_boss), title="Boss", icon=":material/security:")
-    menu_v2_idx_boss_data=st.Page(lambda: menu_tab_show(boss_data), title="Boss data", icon=":material/security:")
+    #menu_v2_idx_boss=st.Page(lambda: menu_tab_show(idx_boss), title="Boss", icon=":material/security:")
+    #menu_v2_idx_boss_data=st.Page(lambda: menu_tab_show(boss_data), title="Boss data", icon=":material/security:")
 
-    menu_v2_m0=[menu_v2_idx_palmon]
-    menu_v2_m50=[menu_v2_idx_costs,menu_v2_idx_comp,menu_v2_idx_mut]
-    menu_v2_m60=[menu_v2_idx_val,menu_v2_idx_equip,menu_v2_idx_equip_nov]
-    menu_v2_m6=[menu_v2_idx_boss,menu_v2_idx_boss_data]
+    #menu_v2_m0=[menu_v2_idx_palmon]
+    #menu_v2_m50=[menu_v2_idx_costs,menu_v2_idx_comp,menu_v2_idx_mut]
+    #menu_v2_m60=[menu_v2_idx_val,menu_v2_idx_equip,menu_v2_idx_equip_nov]
+    #menu_v2_m6=[menu_v2_idx_boss,menu_v2_idx_boss_data]
 
     settings = st.Page(pg_options, title="Settings v2", icon=":material/settings:")
     account_pages = [settings]
     page_dict = {}
     page_dict["Palmons"] = menu_v2_m0
-    page_dict["Calculators"] = menu_v2_m50
-    page_dict["Informations"] = menu_v2_m60
-    page_dict["Boss"] = menu_v2_m6
+    #page_dict["Calculators"] = menu_v2_m50
+    #page_dict["Informations"] = menu_v2_m60
+    #page_dict["Boss"] = menu_v2_m6
 
     if len(page_dict) > 0:
         pg = st.navigation({"Account": account_pages} | page_dict)
