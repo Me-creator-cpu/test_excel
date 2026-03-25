5c15989373bb49cf65c79d1922a379a832f482fb
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-25 08:31:20 +0100

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2323,11 +2323,11 @@ pages = {
 st.session_state.pages_base = pages
 if len(st.session_state.pages_add)>0:
     txt=get_text_trad(site_langu,'menu_myteam')
+    del pages[txt]
     for x in st.session_state.pages_base:
         st.write(x)
         if x == txt:
             st.write('=>to delete')
-            del pages[txt]
     
 pages = st.session_state.pages_base | st.session_state.pages_add
 pg = st.navigation(
