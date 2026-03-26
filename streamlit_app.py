07457688b7d9950b6865af6f4a50609130674a7b
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-26 10:13:13 +0100

    .head(7)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1920,8 +1920,8 @@ def pg_simu_team():
         st.write('Top 7')        
     with row_d3[1]:
         df_result=df.loc[(df["Type"].isin(opt_type)) & (df["Skill"].isin(opt_skill)) & (df["Level"]>0)].copy(deep=True)
-        apply_cols_format(df_result.head(7))
-        build_table_dashboard(df_result,False) 
+        apply_cols_format(df_result)
+        build_table_dashboard(df_result.head(7),False) 
     
     #apply_cols_format(df_result)
     #build_table_dashboard(df_result,False)
