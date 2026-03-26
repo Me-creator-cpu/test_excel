c1aa75f3f9873e1a92e735674679ef1b999a713a
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-26 11:03:30 +0100

    pg_simu_team

--- streamlit_app.py
+++ streamlit_app.py
@@ -1989,11 +1989,11 @@ def pg_simu_team():
         df_t1=df.copy(deep=True)
         apply_cols_format(df_t1)
         build_table_team(df_t1.head(7))
-        df_t1_tcd1=df_t1.groupby(["Type"]).agg("count").reset_index()
+        df_t1_tcd1=df_t1[["Type","Level"]].groupby(["Type"]).agg("count").to_frame('Level').reset_index()
         df_t1_tcd2=df_t1[["Type","Level"]].groupby(["Type"]).agg("mean").reset_index()
         df_t1_tcd1
         df_t1_tcd2
-
+#pg_simu_team
     with row_d4[1]:
         st.write('Team 2')
         df_t2=df_t1.head(14)
