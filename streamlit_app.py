deff8c0dadc0d7f6c5bcad4cdbb501971be9f681
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-27 11:31:51 +0100

    apply_col_skill

--- streamlit_app.py
+++ streamlit_app.py
@@ -1063,7 +1063,7 @@ def get_team_simu(df_ref,teamNb=1):
     st.dataframe(df_t1_tcd2,hide_index=True)    
 
 def apply_col_skill(name,skill):
-    return  name + key_values(skill,data_skill_ico)
+    return  str(name) + str(key_values(skill,data_skill_ico))
 
 def apply_cols_icons(df):
     df['Steps']=df['Step'].apply(lambda b: format_stars(b) )
