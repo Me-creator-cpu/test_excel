c4b4470e310a8ce1e28b86042303b027465d313f
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-31 14:07:24 +0200

    import gv

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ import json
 from user_agents import parse
 import extra_streamlit_components as stx    #https://github.com/Mohamed-512/Extra-Streamlit-Components
 import matplotlib.pyplot as plt
+import gv
 
 from pictures import *
 #sys.path.insert(0, "./tests")  # add "tests" path to search list
