eb90c49b27865cb460566d644b74cb6daf656b37
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-31 16:57:26 +0200

    {color}

--- streamlit_app.py
+++ streamlit_app.py
@@ -2301,7 +2301,7 @@ def build_graph_links(df,parent,child):
         m=df['Mutation 1'][r]
         color=get_cell_value(data_type,"Type","Color",p)
         with debug:
-            st.markdown(f"{color} <span style='color:red'>red</span>",
+            st.markdown(f"{p} <span style='color:{color}'>{color}</span>",
                         unsafe_allow_html=True)
         if color is None:
             graph.edge(p, c)
