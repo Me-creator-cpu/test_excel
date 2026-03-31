f1fbd1b0e060f00699213c4253b06d44f81a9a88
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-31 16:53:15 +0200

    color

--- streamlit_app.py
+++ streamlit_app.py
@@ -2301,7 +2301,7 @@ def build_graph_links(df,parent,child):
         m=df['Mutation 1'][r]
         color=get_cell_value(data_type,"Type","Color",p)
         with debug:
-            st.badge('Test', color='green')
+            st.badge('Test', color=color)
         if color is None:
             graph.edge(p, c)
         else:
