2c59c1524c4c904d345f8e61bcfa2daf327d7673
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-03-31 11:15:13 +0200

    build_graph_links

--- streamlit_app.py
+++ streamlit_app.py
@@ -2248,10 +2248,11 @@ def build_graph_links(df,parent,child):
         p=df_g[parent][r]
         c=df_g[child][r]
         color=get_cell_value(data_type,"Type","Color",p)
+        ico=get_cell_value(data_type,"Type","Icon",p)
         if color is None:
             graph.edge(p, c)
         else:
-            graph.node(p, p, style = "filled", color = color)
+            graph.node(p, ico+p, style = "filled", color = color)
             graph.edge(p, c, style = "filled", color = color)
 
     return st.graphviz_chart(graph)
