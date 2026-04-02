2ef13102a46fa08b1ebd0126b10e020d46d90614
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-04-02 10:17:22 +0200

    get_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -731,7 +731,11 @@ def get_data(file,idx,show_table=False):
                                                 )
     df_xls.loc[idx, "DataFrame"] = data_values
     #df_xls.loc[idx, "DataFrame"]=data_values
-    df_xls.loc[idx, "DataFrame"]
+    row_debug = st.columns(2,border=True, width="stretch")
+    with row_debug[0]:
+        df_xls.loc[idx, "DataFrame"]
+    with row_debug[1]:
+        data_values
 
 def get_data_v0(file,idx,show_table=False):
     # FutureWarning: ChainedAssignmentError: behaviour will change in pandas 3.0!
