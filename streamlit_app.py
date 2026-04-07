1ce680a2cf659259996552a89647a7df7336f7f2
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-04-07 15:02:55 +0200

    comp_cost

--- streamlit_app.py
+++ streamlit_app.py
@@ -1364,7 +1364,7 @@ def pal_deltail(palmon,df,pic_width=300):
         total_comp_costs=int(0)
         for i in [1,2,3,5]:
             comp_cost=int( get_upgrade_comp_costs( df_comp_u.loc[df.index[0], f'Comp {i}'],10 if i==5 else 30 ) )
-            df_comp_u.loc[df.index['0'], f'Comp {str(i)}']=large_num_format(comp_cost)
+            df_comp_u.loc[df.index[int(0)], f'Comp {str(i)}']=large_num_format(comp_cost)
             total_comp_costs=total_comp_costs+comp_cost
             st.write(df_comp_u.loc[df.index[0], f'Comp {str(i)}'])
             st.write(comp_cost)
