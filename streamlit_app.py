f85e30c4f322fbac037d13ef801226332be0c000
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-04-21 11:36:33 +0200

    Level base

--- streamlit_app.py
+++ streamlit_app.py
@@ -1701,11 +1701,12 @@ def menu_tab_mut():
     df = get_df_idx(idx_mut)
     df_energy=get_df_idx(idx_mut)[(df['Step'] != 0)]
     df_crystal=df.loc[(df['Step'] == 0)]  
-    with st.expander('df', expanded=True, width="stretch"):
+    with st.expander('df', expanded=True, width='stretch'):
         df_energy
         df01 = get_df_idx(idx_mut)[(df['Step'] != 0)].set_index('Level').groupby('Level').apply(lambda x: x['Cost level'].sum(), include_groups=False).to_frame('Cost level')
         df01
-        df02=df_energy.groupby(['Level']).sum()
+        df_energy.rename(columns={'Level':'Level base'})
+        df02=df_energy.groupby(['Level base']).sum()
         df02
     st.subheader("🟢Energy")
     #range_level_min, range_level_max = build_chart_bar(df_energy,'Level','Cost level','Mutation costs from level:',int(df_energy['Level'].min()),int(df_energy['Level'].max()))
