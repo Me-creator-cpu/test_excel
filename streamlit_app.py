4409f3a3f9abb803a2950c58898720ee84dbed99
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-05-07 14:32:01 +0200

    Comp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1666,6 +1666,37 @@ def menu_tab_comp():
     
     df = get_df_idx(idx_comp)
     range_level_min, range_level_max = build_chart_bar(df,'Level from','Cost','Competencies costs from level:',int(1),int(30))
+
+    df_pal=get_df_idx(idx_palmon)
+    with st.container(horizontal_alignment="center", 
+                      vertical_alignment="center", 
+                      border=True):
+        nb_pal=st.slider('Nb palmons', min_value=1, max_value=7, value=1, step=1)
+        cost_unit=calcul_upgrade_comp_costs(from_lvl=range_level_min,to_lvl=range_level_max)
+        
+        # 200 / 80 / 40
+        even_ratio=st.selectbox("Points ratio", [200,80,40])
+        event_points=int(cost_unit)/int(even_ratio)
+        cost_nb=int(cost_unit)*int(nb_pal)
+        event_points_nb=int(cost_nb)/int(even_ratio)
+        row1 = st.columns(3,border=False, width="stretch")
+        row2 = st.columns(3,border=False, width="stretch")
+        row3 = st.columns(3,border=False, width="stretch")
+        with row1[0]:
+            st.write(f"{get_text_trad('text_001')} {nb_pal}:")
+        with row1[1]:
+            st.write(large_num_format(cost_nb))
+        with row2[0]:
+            st.write(f"{get_text_trad('text_002')}")
+        with row2[1]:    
+            st.write(large_num_format(event_points))
+        with row3[0]:
+            st.write(f"{get_text_trad('text_003')} {nb_pal}:")
+        with row3[1]:
+            st.write(large_num_format(event_points_nb))
+
+
+
     with st.expander(get_text_trad('data_graph'), expanded=False, width="stretch"):
         build_table_any(df.loc[(df['Level from'] >= range_level_min) & (df['Level from'] <= range_level_max)])
 
