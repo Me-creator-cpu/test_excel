f77f522c278b2a229d86591d1d9540b1b2724e4e
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-05-12 15:08:21 +0200

    Update test_calendar.py

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -363,12 +363,12 @@ if planning.get("eventsSet") is not None:
 if st.button('Ungroup'):
     #planning.resourceGroupField=None
         pass
-
-with st.expander('Calendar eventsSet', expanded=False, icon=':material/table_view:', width='stretch'):
-        #st.write(        planning['eventsSet']['view'].currentEnd[:10]        )
-        initialDate=planning['eventsSet']['view'].currentEnd
+if "currentEnd" in planning['eventsSet']['view']:
+        with st.expander('Calendar eventsSet', expanded=True, icon=':material/table_view:', width='stretch'):
+                #st.write(        planning['eventsSet']['view'].currentEnd[:10]        )
+                initialDate=planning['eventsSet']['view'].currentEnd
         
-with st.expander('Calendar eventsSet', expanded=False, icon=':material/table_view:', width='stretch'):
+with st.expander('Calendar data', expanded=False, icon=':material/table_view:', width='stretch'):
         st.write("Calendar data:", planning)
 
 btn_txt_book='Book a meeting 🗓️'
