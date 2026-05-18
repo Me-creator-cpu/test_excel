5921f4e1b1ea550aeab50a0f570f9b83623562e3
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-05-18 16:27:41 +0200

    Update test_calendar.py

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -252,8 +252,8 @@ def resa_book():
     st.header("New event")
     current_date = current_time_ist.date()
     current_time = current_time_ist.time()
-    office_start_time = datetime.time(9, 0)
-    office_end_time = datetime.time(18, 0)
+    office_start_time = datetime(9, 0).time()
+    office_end_time = datetime(18, 0).time()
     default_time_gap=15
     if default_time_gap < 15:
         default_time_gap=15
