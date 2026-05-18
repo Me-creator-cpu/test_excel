54b709c5b779b914ba1d8e00061db570c48ed210
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-05-18 16:23:28 +0200

    Update test_calendar.py

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -327,13 +327,14 @@ def form_time(item):
 			return ve2
 		return ve1
 
+# ======================================================================================================================================================================================================
 @st.dialog("New Event")
 def new_event(item):
     st.write(f"Why is {item} your favorite?")
     reason = st.text_input("Reason...")
     #options = st.selectbox(f"Filter values for event:", data_cours_niveau['niveau_txt'],index=None)
-    options = st.selectbox(f"Filter values for event:", list(data_cours_niveau),index=None)
-    data_cours_filtered=data_cours.loc[('niveau'>=option-1) & ('niveau'<=option+1)]
+    options = st.selectbox(f"Filter values for event:", data_cours_niveau,index=None)
+    data_cours_filtered=dict(data_cours).loc[('niveau'>=option-1) & ('niveau'<=option+1)]
 #df = df_chart.loc[(df_chart[xField] >= int(selMin)) & (df_chart[xField] <= int(selMax))]
     formatted_cours = [et['title'] + ': ' + form_time(et['start']).strftime('%H:%M:%S') + '-' + form_time(et['end']).strftime('%H:%M:%S') for et in list(data_cours_filtered)]
     requested_cours=st.selectbox("Select the period:", formatted_cours,index=None) 
@@ -348,7 +349,7 @@ if "new_event" not in st.session_state:
         new_event("current_time_ist")
 else:
     f"New event is {st.session_state.new_event['item']} for {st.session_state.new_event['reason']}"
-
+# ======================================================================================================================================================================================================
 
 st.title("Planning")
 
